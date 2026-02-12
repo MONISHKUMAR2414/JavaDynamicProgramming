7d41b32e4148e42b576b20b0f37378d69a06763f
MONISHKUMAR S <131179128+MONISHKUMAR2414@users.noreply.github.com>
2026-02-12 09:56:52 +0530

    Add 4055_longest_balanced_substring_i.java - LeetCode Problem 4055

---
+++ 4055_longest_balanced_substring_i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int[] freq = new int[26];
+
+            for (int j = i; j < n; j++) {
+
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+
+                if (isBalanced(freq)) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+
+    private boolean isBalanced(int[] freq) {
+
+        int count = 0;
+
+        for (int f : freq) {
+            if (f > 0) {
+                if (count == 0) {
+                    count = f;
+                } else if (count != f) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
