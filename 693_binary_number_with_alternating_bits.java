548ca80fcbebc777348d3b2f404fdba242e02adb
MONISHKUMAR S <131179128+MONISHKUMAR2414@users.noreply.github.com>
2026-02-18 10:32:56 +0530

    Add 693_binary_number_with_alternating_bits.java - LeetCode Problem 693

---
+++ 693_binary_number_with_alternating_bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String num = Integer.toBinaryString(n);
+        
+
+        for (int i = 1; i < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
